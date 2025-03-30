2698769d23aa07ac404f4e2084c60663f647ef4b
boltenkov <boltenkov@gmail.com>
2025-03-30 19:33:31 +0300

    Send to GitHub

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-# Модуль ОС для получения данных о файлах
 import os
-# Модуль для текущего времени
 from datetime import datetime
-# МОдуль для вывода имени из командной строки
 import argparse
 
-# Создаем функцию для пасинга
 def createparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--name', default='User')
@@ -16,19 +12,15 @@ parser = createparser()
 namespace = parser.parse_args()
 path = namespace.path
 
-# Выводим приветствие с именем пользователя и текущую дату:
 name = namespace.name
 current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print(f"Hello, {name}!\nCurrent time: {current_datetime}")
 
 
-# Рекурсивный подсчет количества файлов в папках и внутри подпапо
-
 file_count = len([f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))])
 print(f"количество файлов по указанному пути: {file_count}")
 
 
-# Сортировка посчитанных файлов от большего к меньшему и вывод первых 10:
 list_of_files = sorted((os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))),
                    key=lambda f: os.path.getsize(f), reverse=True)[:10]
 print("Top 10 largest files (in KB):")
